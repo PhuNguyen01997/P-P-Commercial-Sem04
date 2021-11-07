38ff439756d816104e7cf40cb6ab69d7851bedda
cba-parker <parker@cybridge.jp>
2021-11-07 16:15:47 +0700

    initial

---
+++ PAndP/src/main/java/PP/PAndP/PAndPApplication.java
@@ -0,0 +1,13 @@
+package PP.PAndP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PAndPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PAndPApplication.class, args);
+	}
+
+}
---
+++ PAndP/src/test/java/PP/PAndP/PAndPApplicationTests.java
@@ -0,0 +1,13 @@
+package PP.PAndP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PAndPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
