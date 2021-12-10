b308827ab0e41afd7a6a26c4621f5978ae995f93
Nguyen Hoang Phu <phunguyen01997@gmail.com>
2021-12-11 00:41:46 +0700

    commit

--- P2P/src/main/java/com/apt/p2p/controller/MainController.java
+++ P2P/src/main/java/com/apt/p2p/controller/MainController.java
@@ -2,6 +2,7 @@ package com.apt.p2p.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -13,5 +14,100 @@ public class MainController {
     @GetMapping("pay")
     public String payment(){
         return "user/main/payment";
+    }
+
+    @GetMapping("shop/{id}")
+    public String shopDetail(@PathVariable int id){
+        return "user/main/shop-detail";
+    }
+
+    @GetMapping("product/{productSlug}")
+    public String productDetail(@PathVariable String productSlug){
+        return "user/main/product-detail";
+    }
+
+    @GetMapping("address")
+    public String address(){
+        return "user/account/address";
+    }
+
+    @GetMapping("card")
+    public String card(){
+        return "user/account/card";
+    }
+
+    @GetMapping("order")
+    public String order(){
+        return "user/account/order-user";
+    }
+
+    @GetMapping("shop/order")
+    public String orderShop(){
+        return "user/account/shop-order";
+    }
+
+    @GetMapping("order/{id}")
+    public String orderDetail(@PathVariable int id){
+        return "user/account/order-detail";
+    }
+
+    @GetMapping("shop/order/{id}")
+    public String orderDetailShop(@PathVariable int id){
+        return "user/account/order-detail";
+    }
+
+    @GetMapping("edit")
+    public String userEdit(){
+        return "user/account/user-form";
+    }
+
+    @GetMapping("cart")
+    public String cart(){
+        return "user/account/cart";
+    }
+
+    @GetMapping("identity")
+    public String identity(){
+        return "user/account/identity";
+    }
+
+    @GetMapping("product")
+    public String products(){
+        return "user/account/product";
+    }
+
+    @GetMapping("product/create")
+    public String productCreate(){
+        return "user/account/product-form";
+    }
+
+    @GetMapping("product/{id}/edit")
+    public String productEdit(@PathVariable int id){
+        return "user/account/product-form";
+    }
+
+    @GetMapping("shop/create")
+    public String shopCreate(){
+        return "user/account/shop-form";
+    }
+
+    @GetMapping("shop/{id}/edit")
+    public String shopEdit(@PathVariable  int id){
+        return "user/account/shop-form";
+    }
+
+    @GetMapping("signup")
+    public String signup(){
+        return "user/auth/signup";
+    }
+
+    @GetMapping("reset-password")
+    public String resetPassword(){
+        return "user/auth/reset";
+    }
+
+    @GetMapping("forgot-password")
+    public String forgotPassword(){
+        return "user/auth/forgot";
     }
 }
