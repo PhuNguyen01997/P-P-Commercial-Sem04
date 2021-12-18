f4393e876c2abcff689c1fc343a0aab0dbfdcb20
cba_parker <parker@cybridge.jp>
2021-12-18 21:38:41 +0700

    commit

--- P2P/src/main/java/com/apt/p2p/common/SeedData.java
+++ P2P/src/main/java/com/apt/p2p/common/SeedData.java
@@ -3,6 +3,8 @@ package com.apt.p2p.common;
 import com.apt.p2p.entity.User;
 import com.apt.p2p.repository.UserRepository;
 import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.jeasy.random.FieldPredicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -10,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SeedData {
-    @Autowired
-    private EasyRandom easyRandom;
+//    @Autowired
+//    private EasyRandom easyRandom;
     @Autowired
     private UserRepository userRepository;
 
@@ -21,6 +23,12 @@ public class SeedData {
     }
 
     private void seedUser(int number){
+        EasyRandom easyRandom = new EasyRandom();
+        EasyRandomParameters easyRandomParameters = new EasyRandomParameters()
+                .excludeField(FieldPredicates.named("avatar").and(FieldPredicates.ofType(String.class)).and(FieldPredicates.inClass(User.class)))
+                .stringLengthRange(3, 10);
+        User user = easyRandom.nextObject(User.class);
+        userRepository.save(user);
 //        for (int i = 0; i < number; i++) {
 //            easyRandom.nextObject(User.class);
 //        }
