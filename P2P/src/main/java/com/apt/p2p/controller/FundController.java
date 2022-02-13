a2a8bd5d7b6461f6f955c4edf4d63790eaeb40fc
Nguyen Hoang Phu <phunguyen01997@gmail.com>
2022-02-14 00:13:19 +0700

    commit

--- P2P/src/main/java/com/apt/p2p/controller/FundController.java
+++ P2P/src/main/java/com/apt/p2p/controller/FundController.java
@@ -1,5 +1,7 @@
 package com.apt.p2p.controller;
 
+import com.apt.p2p.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FundController {
+    @Autowired
+    private ShopService shopService;
+
     @GetMapping("/portal/{id}/fund")
     public String portalIndex(Model model, @PathVariable("id") int shopId) {
+        model.addAttribute("shop", shopService.findById(shopId));
         return "user/portal/fund";
+    }
+
+    @GetMapping("/portal/{id}/fund/withdraw")
+    public String portalWithdraw(Model model, @PathVariable("id") int shopId) {
+        model.addAttribute("shop", shopService.findById(shopId));
+        return "user/portal/fund-withdraw";
     }
 }
