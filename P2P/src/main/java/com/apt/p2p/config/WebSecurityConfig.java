a54b182de5ef866a8d223d6cea7de84c85b6934f
Nguyen Hoang Phu <phunguyen01997@gmail.com>
2022-04-19 20:53:51 +0700

    commit

--- P2P/src/main/java/com/apt/p2p/config/WebSecurityConfig.java
+++ P2P/src/main/java/com/apt/p2p/config/WebSecurityConfig.java
@@ -99,7 +99,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/singin").permitAll()
+                .logoutSuccessUrl("/signin").permitAll()
                 .and()
                 .authorizeRequests().and().rememberMe()
                 .tokenRepository(persistenceTokenRepository())
