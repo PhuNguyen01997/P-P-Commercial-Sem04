7638ee5fbd14eacc7da9a5a79137e36bdd836397
Nguyen Hoang Phu <phunguyen01997@gmail.com>
2022-04-21 13:06:07 +0700

    commit

--- P2P/src/main/java/com/apt/p2p/config/WebSecurityConfig.java
+++ P2P/src/main/java/com/apt/p2p/config/WebSecurityConfig.java
@@ -108,9 +108,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().accessDeniedPage("/403");
         http.sessionManagement()
                 .invalidSessionUrl("/reset?token=invalid");
-        http.sessionManagement()
-                .maximumSessions(1);
-
     }
 
     @Bean
